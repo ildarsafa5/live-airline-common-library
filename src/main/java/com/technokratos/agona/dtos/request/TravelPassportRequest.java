1ed15b363b8aeec66c502ab2b1dfac9aa9eceac3
ildarsafa5 <ildarsafa5@gmail.com>
2026-04-12 01:29:40 +0300

    fix

--- src/main/java/com/technokratos/agona/dtos/request/TravelPassportRequest.java
+++ src/main/java/com/technokratos/agona/dtos/request/TravelPassportRequest.java
@@ -11,4 +11,37 @@ public class TravelPassportRequest {
     private LocalDate dateOfIssue;
     @JsonFormat(pattern = "dd.MM.yyyy")
     private LocalDate dateOfExpiry;
+
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDate getDateOfExpiry() {
+        return dateOfExpiry;
+    }
+
+    public void setDateOfExpiry(LocalDate dateOfExpiry) {
+        this.dateOfExpiry = dateOfExpiry;
+    }
+
+    public LocalDate getDateOfIssue() {
+        return dateOfIssue;
+    }
+
+    public void setDateOfIssue(LocalDate dateOfIssue) {
+        this.dateOfIssue = dateOfIssue;
+    }
 }
