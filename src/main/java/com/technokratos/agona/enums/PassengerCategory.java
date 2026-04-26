cdf66364ee4069789db3089280a6199fae9dd7cd
vanish <ivanmaxi52@gmail.com>
2026-04-26 21:57:47 +0300

    edit PassengerCategory

--- src/main/java/com/technokratos/agona/enums/PassengerCategory.java
+++ src/main/java/com/technokratos/agona/enums/PassengerCategory.java
@@ -1,7 +1,16 @@
 package com.technokratos.agona.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@RequiredArgsConstructor
 public enum PassengerCategory {
-    ADULT,
-    CHILD,
-    INFANT
+    ADULT(new BigDecimal("1.00")),
+    CHILD(new BigDecimal("0.75")),
+    INFANT(new BigDecimal("0.10"));
+
+    private final BigDecimal priceCoefficient;
 }
