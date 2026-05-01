3c1896070ea12b81cf622180e7a7287593224596
vanish <ivanmaxi52@gmail.com>
2026-05-01 20:10:16 +0300

    add DisplayName to seatclass

--- src/main/java/com/technokratos/agona/enums/SeatClass.java
+++ src/main/java/com/technokratos/agona/enums/SeatClass.java
@@ -1,6 +1,16 @@
 package com.technokratos.agona.enums;
 
 public enum SeatClass {
-    ECONOMY,
-    BUSINESS
+    ECONOMY("Эконом"),
+    BUSINESS("Бизнес");
+
+    private final String displayName;
+
+    SeatClass(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
