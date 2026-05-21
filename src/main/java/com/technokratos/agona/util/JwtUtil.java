e3db89c42431fa37bba77aa488879b3f5e4f440d
ildarsafa5 <ildarsafa5@gmail.com>
2026-05-22 02:35:49 +0300

    changed secret to public key

--- src/main/java/com/technokratos/agona/util/JwtUtil.java
+++ src/main/java/com/technokratos/agona/util/JwtUtil.java
@@ -5,24 +5,23 @@ import com.technokratos.agona.enums.Role;
 import com.technokratos.agona.exceptions.InvalidAccessTokenException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import javax.crypto.SecretKey;
-import java.time.Instant;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Date;
 import java.util.UUID;
 
 public class JwtUtil {
 
-    private static Claims parseJwtToken(String jwtToken,SecretKey secretKey) {
+    private static Claims parseJwtToken(String jwtToken, RSAPublicKey publicKey) {
         return Jwts.parser()
-                .verifyWith(secretKey)
+                .verifyWith(publicKey)
                 .build()
                 .parseSignedClaims(jwtToken)
                 .getPayload();
     }
 
-    public static Claims validateToken(String jwtToken,SecretKey secretKey) {
+    public static Claims validateToken(String jwtToken,RSAPublicKey publicKey) {
         try {
-            Claims claims = parseJwtToken(jwtToken,secretKey);
+            Claims claims = parseJwtToken(jwtToken,publicKey);
             if (claims.getExpiration().before(new Date())) {
                 throw new InvalidAccessTokenException("Token is expired");
             }
@@ -32,8 +31,8 @@ public class JwtUtil {
         }
     }
 
-    public static AccountResponse extractUser(String jwtToken, SecretKey secretKey) {
-        Claims claims = parseJwtToken(jwtToken,secretKey);
+    public static AccountResponse extractUser(String jwtToken, RSAPublicKey publicKey) {
+        Claims claims = parseJwtToken(jwtToken,publicKey);
         AccountResponse accountResponse = new AccountResponse();
         accountResponse.setAccountId(UUID.fromString(claims.getSubject()));
         accountResponse.setEmail(claims.get("email", String.class));
